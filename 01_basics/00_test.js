f34d53c15eb12c9614a342026b5a518a34a02967
shivamhota724 <23ece047@jssaten.ac.in>
2026-02-16 22:12:46 +0530

    package

--- 01_basics/00_test.js
+++ 01_basics/00_test.js
@@ -1,1 +1,9 @@
 console.log("hotaji")
+
+let str1 = "kjhf";
+let str2 = "kjhf";
+const areEqualInLowerCase = (str1, str2) => {
+  return str1.toLowerCase() === str2.toLowerCase();
+};
+
+console.log(areEqualInLowerCase(str1, str2));
